4a1aae47163027cd18f1e669ecd3522baeb2a767
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2024-04-15 20:17:29 -0300

    Update conversao.py

--- conversao.py
+++ conversao.py
@@ -1,1 +1,11 @@
+#Faça um Programa que converta metros para centímetros
+#1 metro tem 100 centimetros
 
+print('-'*100)
+print('Programa que converte metros em centímetros:')
+print('-'*100)
+
+metro = float(input('Digite a metragem: '))
+centimetro = metro * 100
+
+print(f'A metragem informada -> {metro} equivale a {centimetro} centímetros')
