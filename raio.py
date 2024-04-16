b394e5ec92e5ea0e3ea92ef491f5b71171dc4b46
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2024-04-15 21:40:47 -0300

    Update raio.py

--- raio.py
+++ raio.py
@@ -1,1 +1,21 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+# area = pi*r²
+print('-'*100)
+print('Calculando a área de um circulo')
+print('-'*100)
+
+#inicializando as variaveis
+PI = 3.14
+raio = 0
+area = 0.0
+
+#coletando dados do usuario
+raio = int(input('Informe o valor do raio: '))
+
+#calculando o valor da area
+area = PI*raio**2
+
+#Imprimindo na tela
+
+print(f'A área do círculo é {area}')
 
